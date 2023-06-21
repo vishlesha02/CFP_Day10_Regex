d70792ccd3b1d57f317d3290c1e96b5c8881023c
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-21 22:58:08 +0530

    Last Name Added

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
 ﻿using RegexRegistration;
+using System.Globalization;
 
 public class Program
 {
     public static void Main(String[] args)
     {
-
         Registration uc1 = new Registration();
+        
         uc1.FirstName();
-    
+
+        uc1.LastName();
+
+
+
     }
 
 }
--- Registration.cs
+++ Registration.cs
@@ -21,15 +21,36 @@ namespace RegexRegistration
             if (obj.IsMatch(firstname))
             {
                 Console.WriteLine($"firstname : {firstname} is a valid Name");
-
             }
             else 
             {
                 Console.WriteLine($"firstname : {firstname} is not a valid Name");
-
             }
-
-
         }
+
+
+        private String pattern2 = @"^[A-Z][a-z]{2,}$";
+
+        public void LastName() 
+        {
+
+        Console.WriteLine("Enter Last Name");
+            String lastname = Console.ReadLine();
+
+            Regex obj2=new Regex(pattern2);
+
+
+            if (obj2.IsMatch(lastname))
+            {
+                Console.WriteLine($"Last Name : {lastname} is valid ");
+            }
+        
+            else 
+            {
+                Console.WriteLine($"Last Name : {lastname} is not valid "); 
+            }
+        }
+
+
     }
 }
